0edd5500b31395ec767f950e5aa5a693809a8786
Johannes C. Meyer <alaskanloops@gmail.com>
2016-05-18 13:57:54 -0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+/**
+ * Created by: Johannes Meyer 2016
+ * Many bootstrap functions are opt-in for performance reasons. Uncomment those required.
+ * Documentation for advanced bootstrap functionalities found at https://v4-alpha.getbootstrap.com/components/[FUNCTION]/
+ */
+//
+// // un-comment for popover functionality
+// $(function () {
+//     $('[data-toggle="popover"]').popover()
+// });
+
+// // un-comment for tooltip functionality
+// $(function () {
+//     $('[data-toggle="tooltip"]').tooltip()
+// });
+
+// // un-comment for scrollspy functionality
+// $("body").scrollspy({
+//     target: "#navbar-example"
+// });
